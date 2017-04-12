b395478d5c89690ce1891c100d9cb26fbf282eed
Alvaro Gili <alvarogili@gmail.com>
2017-04-11 21:18:33 -0300

    anda if pero no if-else

--- src/main/java/ar/edu/unrc/asp/cfgbuilder/Main.java
+++ src/main/java/ar/edu/unrc/asp/cfgbuilder/Main.java
@@ -5,6 +5,7 @@ import ar.edu.unrc.asp.cfgbuilder.parser.Parser;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java_cup.runtime.Symbol;
 
 /**
  *
@@ -16,11 +17,13 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException, Exception {
-        java_cup.runtime.Scanner scanner = new LexicalParser(new FileReader(new File("src/test/resources/asign")));
+        java_cup.runtime.Scanner scanner = new LexicalParser(new FileReader(new File("src/test/resources/if_sentence")));
         Parser parser = new Parser(scanner);
-        parser.parse();
-        CFGBuilder cFGBuilder = parser.getCfgBuilder();
-        cFGBuilder.getStartNode();
+        Symbol s = parser.parse();
+        Node startNode = (Node) s.value;
+        CFGBuilder cFGBuilder = new CFGBuilder();
+        cFGBuilder.setStartNode(startNode);
+        System.out.println(cFGBuilder.getStartNode().getName());
     }
 
 }
